0327f226b66a06ddc8db6cf2e897465e60f8d8e0
TwentySixB26 <srijawamusaitomo@gmail.com>
2023-11-27 16:31:02 +0700

    memodifikasi isi landing page dan membuat folder about jkt

--- About Jkt/script.js
+++ About Jkt/script.js
@@ -2,9 +2,6 @@ const menuToogle = document.querySelector('#menuToogle')
 let navbar = document.querySelector(' nav .navbar ul')
 let closeNav = document.querySelector('.closeNav')
 
-// load animation 
-let deksripsi = document.querySelector('.hero .container .deksripsi')
-let judul = document.querySelector('.hero .container .judul')
 
 menuToogle.addEventListener('click', function() {
     navbar.classList.toggle('muncul')
@@ -12,10 +9,4 @@ menuToogle.addEventListener('click', function() {
 
 closeNav.addEventListener('click', function() {
     navbar.classList.toggle('muncul')
-})
-
-// load animation
-window.addEventListener('load', function() {
-    deksripsi.classList.add('muncul')
-    judul.classList.add('muncul')
 })
