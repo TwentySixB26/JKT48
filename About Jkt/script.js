af0f0829b75013f2b166770ded6e36174a71a91c
TwentySixB26 <srijawamusaitomo@gmail.com>
2023-11-29 20:25:47 +0700

    memodifikasi isi folder about jkt48

--- About Jkt/script.js
+++ About Jkt/script.js
@@ -2,12 +2,6 @@ const menuToogle = document.querySelector('#menuToogle')
 let navbar = document.querySelector(' nav .navbar ul')
 let closeNav = document.querySelector('.closeNav')
 
-// carousel 
-let prevBtn = document.querySelector('section.about1 .container .carousel .prev-btn')
-let imgCarousel = document.querySelector('section.about1 .container .carousel .img img')
-let nextBtn = document.querySelector('section.about1 .container .carousel .next-btn')
-
-// akhir carousel 
 
 menuToogle.addEventListener('click', function() {
     navbar.classList.toggle('muncul')
@@ -16,18 +10,3 @@ menuToogle.addEventListener('click', function() {
 closeNav.addEventListener('click', function() {
     navbar.classList.toggle('muncul')
 })
-
-
-// carosel 
-prevBtn.addEventListener('click', function() {
-    imgCarousel.setAttribute('src', '../img/feed2.jpg')
-    imgCarousel.style.transition = "ease 0.9s";
-
-})
-
-nextBtn.addEventListener('click', function() {
-        imgCarousel.setAttribute('src', '../img/about jkt 2.jpg')
-        imgCarousel.style.transition = "ease 0.9s";
-
-    })
-    //akhir carousel
