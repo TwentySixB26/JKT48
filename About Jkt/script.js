0218715634bf77151e02da1b25c406cabaf07fb9
TwentySixB26 <srijawamusaitomo@gmail.com>
2023-12-04 00:30:51 +0700

    memodifikasi isi about jkt48

--- About Jkt/script.js
+++ About Jkt/script.js
@@ -1,6 +1,12 @@
 const menuToogle = document.querySelector('#menuToogle')
 let navbar = document.querySelector(' nav .navbar ul')
 let closeNav = document.querySelector('.closeNav')
+let imgAbout1 = document.querySelector('section.about1 .container .img')
+let dekskripsiAbout1 = document.querySelector('section.about1 .container .deksripsi')
+
+let arrowRight = document.querySelector('section.about3 .container .story2 .arrowRight ')
+let story3 = document.querySelector('section.about3 .container .story3')
+
 
 
 menuToogle.addEventListener('click', function() {
@@ -9,4 +15,15 @@ menuToogle.addEventListener('click', function() {
 
 closeNav.addEventListener('click', function() {
     navbar.classList.toggle('muncul')
+})
+
+// load animation
+window.addEventListener('load', function() {
+    imgAbout1.classList.add('muncul')
+    dekskripsiAbout1.classList.add('muncul')
+})
+
+//arow right click
+arrowRight.addEventListener('click', function() {
+    story3.classList.add('muncul')
 })
