30a538d1cbcff2587d5f9ad5c15d3b4408136e2c
TwentySixB26 <srijawamusaitomo@gmail.com>
2023-12-08 10:47:54 +0700

    membuat dan memodifikasi isi folder member jkt48

---
+++ member/script.js
@@ -0,0 +1,186 @@
+const menuToogle = document.querySelector('#menuToogle')
+let navbar = document.querySelector(' nav .navbar ul')
+let closeNav = document.querySelector('.closeNav')
+
+
+menuToogle.addEventListener('click', function() {
+    navbar.classList.toggle('muncul')
+})
+
+closeNav.addEventListener('click', function() {
+    navbar.classList.toggle('muncul')
+})
+
+
+
+// voice
+let ashel = document.querySelector('section.member .container .isi2 ul li .ashel')
+ashel.addEventListener('click', function() {
+    let audio = new Audio('../voice/ashel.mp3')
+    audio.play()
+})
+
+
+
+let freya = document.querySelector('section.member .container .isi2 ul li .freya')
+freya.addEventListener('click', function() {
+    let audio = new Audio('../voice/freya.mp3')
+    audio.play()
+})
+
+
+let marsha = document.querySelector('section.member .container .isi2 ul li .marsha')
+marsha.addEventListener('click', function() {
+    let audio = new Audio('../voice/marsha.mp3')
+    audio.play()
+})
+
+let christy = document.querySelector('section.member .container .isi2 ul li .christy')
+christy.addEventListener('click', function() {
+    let audio = new Audio('../voice/christy.mp3')
+    audio.play()
+})
+
+let helisma = document.querySelector('section.member .container .isi2 ul li .helisma')
+helisma.addEventListener('click', function() {
+    let audio = new Audio('../voice/helisma.mp3')
+    audio.play()
+})
+
+let fiony = document.querySelector('section.member .container .isi2 ul li .fiony')
+fiony.addEventListener('click', function() {
+    let audio = new Audio('../voice/fiony.mp3')
+    audio.play()
+})
+
+let indah = document.querySelector('section.member .container .isi2 ul li .indah')
+indah.addEventListener('click', function() {
+    let audio = new Audio('../voice/indah.mp3')
+    audio.play()
+})
+
+let lulu = document.querySelector('section.member .container .isi2 ul li .lulu')
+lulu.addEventListener('click', function() {
+    let audio = new Audio('../voice/lulu.mp3')
+    audio.play()
+})
+
+let olla = document.querySelector('section.member .container .isi2 ul li .olla')
+olla.addEventListener('click', function() {
+    let audio = new Audio('../voice/olla.mp3')
+    audio.play()
+})
+
+let oniel = document.querySelector('section.member .container .isi2 ul li .oniel')
+oniel.addEventListener('click', function() {
+    let audio = new Audio('../voice/oniel.mp3')
+    audio.play()
+})
+
+let zee = document.querySelector('section.member .container .isi2 ul li .zee')
+zee.addEventListener('click', function() {
+    let audio = new Audio('../voice/zee.mp3')
+    audio.play()
+})
+
+let flora = document.querySelector('section.member .container .isi2 ul li .flora')
+flora.addEventListener('click', function() {
+    let audio = new Audio('../voice/flora.mp3')
+    audio.play()
+})
+
+let raisha = document.querySelector('section.member .container .isi2 ul li .raisha')
+raisha.addEventListener('click', function() {
+    let audio = new Audio('../voice/raisha.mp3')
+    audio.play()
+})
+
+let jessi = document.querySelector('section.member .container .isi2 ul li .jessi')
+jessi.addEventListener('click', function() {
+    let audio = new Audio('../voice/jessi.mp3')
+    audio.play()
+})
+
+let mutthe = document.querySelector('section.member .container .isi2 ul li .mutthe')
+mutthe.addEventListener('click', function() {
+    let audio = new Audio('../voice/mutthe.mp3')
+    audio.play()
+})
+
+let feni = document.querySelector('section.member .container .isi2 ul li .feni')
+feni.addEventListener('click', function() {
+    let audio = new Audio('../voice/feni.mp3')
+    audio.play()
+})
+
+let calista = document.querySelector('section.member .container .isi2 ul li .calista')
+calista.addEventListener('click', function() {
+    let audio = new Audio('../voice/calista.mp3')
+    audio.play()
+})
+
+let indira = document.querySelector('section.member .container .isi2 ul li .indira')
+indira.addEventListener('click', function() {
+    let audio = new Audio('../voice/indira.mp3')
+    audio.play()
+})
+
+let amanda = document.querySelector('section.member .container .isi2 ul li .amanda')
+amanda.addEventListener('click', function() {
+    let audio = new Audio('../voice/amanda.mp3')
+    audio.play()
+})
+
+let adel = document.querySelector('section.member .container .isi2 ul li .adel')
+adel.addEventListener('click', function() {
+    let audio = new Audio('../voice/adel.mp3')
+    audio.play()
+})
+
+let shani = document.querySelector('section.member .container .isi2 ul li .shani')
+shani.addEventListener('click', function() {
+    let audio = new Audio('../voice/shani.mp3')
+    audio.play()
+})
+
+let yessica = document.querySelector('section.member .container .isi2 ul li .yessica')
+yessica.addEventListener('click', function() {
+    let audio = new Audio('../voice/yessica.mp3')
+    audio.play()
+})
+
+let lia = document.querySelector('section.member .container .isi2 ul li .lia')
+lia.addEventListener('click', function() {
+    let audio = new Audio('../voice/lia.mp3')
+    audio.play()
+})
+
+let lyn = document.querySelector('section.member .container .isi2 ul li .lyn')
+lyn.addEventListener('click', function() {
+    let audio = new Audio('../voice/lyn.mp3')
+    audio.play()
+})
+
+let kathrina = document.querySelector('section.member .container .isi2 ul li .kathrina')
+kathrina.addEventListener('click', function() {
+    let audio = new Audio('../voice/kathrina.mp3')
+    audio.play()
+})
+
+let gita = document.querySelector('section.member .container .isi2 ul li .gita')
+gita.addEventListener('click', function() {
+    let audio = new Audio('../voice/gita.mp3')
+    audio.play()
+})
+
+let gracia = document.querySelector('section.member .container .isi2 ul li .gracia')
+gracia.addEventListener('click', function() {
+    let audio = new Audio('../voice/gracia.mp3')
+    audio.play()
+})
+
+let ela = document.querySelector('section.member .container .isi2 ul li .ela')
+ela.addEventListener('click', function() {
+    let audio = new Audio('../voice/ela.mp3')
+    audio.play()
+})
